ae679d56041cd16308dbd23ab6718096641c298e
juldiadia <julian.diaz@upinion.com>
2015-11-04 15:04:47 +0100

    Fixed package dir.

--- src/main/java/com/LinkingAndroid/LinkingAndroid.java
+++ src/main/java/com/LinkingAndroid/LinkingAndroid.java
@@ -1,4 +1,4 @@
-package com.app;
+package com.upinion.LinkingAndroid;
 
 import android.content.Intent;
 import android.content.Context;
--- src/main/java/com/LinkingAndroid/LinkingAndroidPackage.java
+++ src/main/java/com/LinkingAndroid/LinkingAndroidPackage.java
@@ -1,4 +1,4 @@
-package com.app;
+package com.upinion.LinkingAndroid;
 
 import java.util.*;
 
